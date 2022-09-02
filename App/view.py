166cf7b933423d3df807b06dedd55f5796880ea4
nevs245 <nevitaaguirrec@gmail.com>
2022-09-02 07:32:08 -0500

    Entrega final - laboratorio 4

--- App/view.py
+++ App/view.py
@@ -16,7 +16,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU General P5ublic License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  * Contribuciones
  *
